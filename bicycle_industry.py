c78eaccea8428817ec102ef7f46e6f89d883400a
Olzhas Shaikenov <olzhas.shaikenov@gmail.com>
2014-10-15 17:59:59 -0400

    Bike shop project

--- bicycle_industry.py
+++ bicycle_industry.py
@@ -1,30 +1,8 @@
 __author__ = 'Olzhas'
+import Bicycle
+import frames
+import wheels
 
-
-class Bicycle:
-
-    def __init__(self, model_name,weight,produce_cost):
-        self.model_name=model_name
-        self.weight=weight
-        self.produce_cost=produce_cost
-
-        return
-
-   #class Wheels(Bicycle):
-
-    #class Frames(Bicycle):
-
-
-
-class Bike_Shops:
-
-    def __init__(self,name,inventory,margin):
-        self.name=name
-        self.inventory=inventory
-        self.margin=margin
-
-        self.price=(self.inventory*self.margin)/self.inventory
-        print self.price
 
 
 class Customers:
@@ -34,24 +12,97 @@ class Customers:
         self.fund_of_money=fund_of_money
         self.new_bike=True
 
+class Bike_Shops(object):
+
+      #"""inventory is the dictionary with names and prices of the inventory we have"""
+      # bike shop takes name, inventory and margin and returns the profit
+
+    def __init__(self,name, margin,*kwargs):
+        
+        self.name=name
+        self.margin=margin
+        
+        
+   
+        
+    def prices(self):
+
+        name_price = { }
+    
+
+        for name, cost in inventory.iteritems():
+            
+            name_price[name]=(cost*self.margin)/100+cost
+        
+        self.price=name_price
+        return self.price   
+    
+
+
+class Shop_Inventory(Bike_Shops):
+
+    def  inventory(self, key):
+        new_inventory={ }
+
+        for name, cost in inventory.iteritems():
+            print inventory[name]
+            print inventory[cost]
+            
+            if new_inventory[name]==key:
+                    del(new_inventory[name])
+            else:
+                return new_inventory
+        return new_inventory
+        pass
+      
+def afford(customer_name):
+        
+        
+        
+        for name, price in my_bike_shop.prices().iteritems():
+            
+            
+            
+
+            if customer_name.fund_of_money>=price:
+                print "______________________________________"
+                print ("Dear %s we have %s for $%s\n" %(customer_name.name,name, price))
+                customer_name.fund_of_money=(customer_name.fund_of_money-price)
+                print ("you bought %s and have $ %s left" %(name, customer_name.fund_of_money))
+                                              
+                
+                print "______________________________________"
+                profit.append(price-inventory[name])
+                print ("Bike shop profit is $%s" % sum(profit))
+
+                sold_bikes[name]=inventory[name]
+                print "Sold to %s %s: " %(customer_name.name, sold_bikes)
+                return sold_bikes
+            else:
+                pass
+      
 
 
 
-my_bike_shop=Bike_Shops("Bikes_for_evr1", 6, 20)
 
-print ("Bike shop name is: ", my_bike_shop.name)
-print ("It has :  bikes", my_bike_shop.inventory)
+profit=[]
+remaining_inventory={ }
+sold_bikes={ }
 
-customer1=Customers("Mike", 200, True)
+inventory={'Giant':200, 'GT':250, 'Bianchi':500, 'Shwinn':500}
+
+
+my_bike_shop=Bike_Shops("Bikes for Everyone",20, inventory)
+
+
+
+customer1=Customers("Mike", 245, True)
 customer2=Customers("Eve", 500, True)
 customer3=Customers("Bob", 1000, True)
 
-print customer1.name
-
-if customer1.fund_of_money >= my_bike_shop.price:
-    print ("This is how many bikes  %s you can buy from %s" %(customer1.name,my_bike_shop.name))
-
-
+afford(customer1)
+afford(customer2)
+afford(customer3)
 
 
 
