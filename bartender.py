52771d2645b01159fb9c87405f761f1e1fb75f97
Olzhas Shaikenov <olzhas.shaikenov@gmail.com>
2014-10-16 02:33:13 -0400

    Bartender Project

--- bartender.py
+++ bartender.py
@@ -15,54 +15,94 @@ ingredients = {
     "fruity": ["slice of orange", "dash of cassis", "cherry on top"]
 }
 
-coctail_names = { "Fluffy Chinchilla" : ["Dog"], 
-                  "Salty": [ "Sea-Dog"],
-                  "Fluffy": [ "Sea-Cow"]
+coctail_names = { "Fluffy Chinchilla" : ["Dogydog","Cat-Smile"], 
+                  "Salty":  ["Seawater", "Sea-mouse"],
+                  "Fluffy": ["Splash", "Old Style"]
     
 }
 
-answers_set={}
+answers={}
 
 def style_drink():
 
     """ Here we ask the customer what kind of drink he/she likes"""
     for flavor, question in questions.iteritems():
         print question, " Yes/y or not/ n?"
-        answers_set[flavor]=raw_input().lower()
+        answers[flavor]=raw_input().lower()
+        
 
-    return answers_set
+    return answers
 
 
 
 
-def constract_drink(answers_set):
+def constract_drink(answers):
     drink = []
-    for ingredient_type, liked in answers_set.iteritems():
-        if not liked:
-            continue
- 
-        drink.append(random.choice(ingredients[ingredient_type]))
+    for ingredient_type, liked in answers.iteritems():
+        if liked=="yes" or liked=="y":
+             drink.append(random.choice(ingredients[ingredient_type]))
+        else:
+            pass
+        
     return drink
 
-"""def coctail_name():
+def coctail_name(drink):
     drink_name = []
-    for keys, values in coctail_names.iteritems:
-        drink_name.append(random.choice(coctail_names[values]))
+    for keys, values in coctail_names.iteritems():
+        drink_name.append(random.choice(coctail_names[keys]))
+        
    
-    return drink_name"""
+    return drink_name
 
-
+def customer_name():
+    
+    print "What is your name?"
+    customer=raw_input().lower()
+    return customer
 
 
 
 def main():
-    answers_set = style_drink()
-    drink = constract_drink(answers_set)
-   # d_name=coctail_name()
-    print "Your drink"
-    #print d_name
+    
+    drink_name=""
+    
+    count=0
+    answers = style_drink()
+    drink = constract_drink(answers)
+    drink_name=coctail_name(drink)
+
+    
+    
+
+
+
+    while count<num_customers:
+                        customer=customer_name()
+                        count+=1
+                        
+
+    print "Your drink name is "
+
+    while count==0:
+                    print (''.join(map(str,random.choice(drink_name))))
+                    count +=1
+    
+
+        
     for ingredient in drink:
-        print "A {}".format(ingredient)
- 
+                print "A {}".format(ingredient)
+         
 if __name__ == "__main__":
-    main()
+    customer={}    
+    
+    
+    count_m=0
+
+    print "How many customers in the bar right now?"
+    num_customers=int(raw_input())
+
+    while count_m<num_customers:
+
+                        main()
+                        count_m+=1
+
