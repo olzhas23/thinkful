e137e226a1cae35e48dc43fe2addf863432a5757
Olzhas Shaikenov <olzhas.shaikenov@gmail.com>
2014-10-15 23:00:28 -0400

    Snippets-app

--- snippets-app/snippets.py
+++ snippets-app/snippets.py
@@ -7,5 +7,9 @@ def put(name,filename,snippet):
 	    logging.info("Writing {!r}:{!r} to {!r}".format(name, snippet, filename))
     	logging.debug("Opening file")
     	with open(filename, "a") as f:
-        	writer = csv.writer(f)
-
+	       	writer = csv.writer(f)
+       	
+        	logging.debug("Writing snippet to file")
+        	writer.writerow([name, snippet])
+    logging.debug("Write sucessful")
+    return name, snippet
